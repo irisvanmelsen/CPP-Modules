7e4f07aeba277a47af255aa894dbf70c811eb577
Iris van Melsen <iris@MBP-van-Iris.home>
2024-07-11 21:23:49 +0200

    working part 1

--- CPP_Module_09/ex00/src/main.cpp
+++ CPP_Module_09/ex00/src/main.cpp
@@ -17,12 +17,3 @@ int main (int argc, char **argv) {
 		std::cout << "The program needs ./btc and an input file to work!" << std::endl;
 	}
 }
-=======
-		BitcoinExchange program;
-
-		program.GoThroughData(argv[1]);
-	}
-	else
-		std::cout << "Needs 2 arguments: ./btc and a database file in csv format!" << std::endl;
-}
->>>>>>> 42fa59ea46b4358f2e186843b7bc2e236329a847
